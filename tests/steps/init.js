bd6a34abd6a1ac494fa2847f13e0c2b22e7e6b46
Andrew Miko <andrew.miko@yandex.com>
2018-12-01 14:49:26 +0200

    Added ssm

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,16 +1,46 @@
+const _ = require('lodash')
+const { promisify } = require('util')
+const awscred = require('awscred')
+const { REGION, STAGE } = process.env
+const AWS = require('aws-sdk')
+AWS.config.region = REGION
+const SSM = new AWS.SSM()
+
 let initialized = false
+
+const getParameters = async (keys) => {
+  const prefix = `/workshop-devternity/${STAGE}/`
+  const req = {
+    Names: keys.map(key => `${prefix}${key}`)
+  }
+  const resp = await SSM.getParameters(req).promise()
+  return _.reduce(resp.Parameters, function(obj, param) {
+    obj[param.Name.substr(prefix.length)] = param.Value
+    return obj
+   }, {})
+}
 
 const init = async () => {
   if (initialized) {
     return
   }
 
-  process.env.AWS_REGION = "us-east-1"
-  process.env.TEST_ROOT = "https://qo0nqso709.execute-api.us-east-1.amazonaws.com/dev"
-  process.env.restaurants_api   = "https://qo0nqso709.execute-api.us-east-1.amazonaws.com/dev"
-  process.env.restaurants_table = "restaurants-dev-devternity"
-  process.env.order_events_stream = 'orders-dev-devternity'
-  process.env.restaurant_notification_topic = 'restaurants-dev-devternity'
+  const params = await getParameters([
+    'table_name',
+    'stream_name',
+    'restaurant_topic_name',
+    'url'
+  ])
+
+  console.log('SSM params loaded')
+
+  process.env.TEST_ROOT                     = params.url
+  process.env.AWS_REGION                    = REGION
+  process.env.orders_api                    = `${params.url}/orders`
+  process.env.restaurants_api               = `${params.url}/restaurants`
+  process.env.restaurants_table             = params.table_name
+  process.env.order_events_stream           = params.stream_name
+  process.env.restaurant_notification_topic = params.restaurant_topic_name
   
   initialized = true
 }
